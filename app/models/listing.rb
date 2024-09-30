b5232d37c3516fc25f75673fb014c5b8482871f8
David Cornu <me@davidcornu.com>
2024-09-30 14:34:01 -0400

    Add `Listing.overview`

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,4 +7,30 @@
 #
 class Listing < ApplicationRecord
   has_many :products
+
+  def self.overview
+    grouped_properties = ProductProperty
+      .joins(:property)
+      .group(:product_id)
+      .select(
+        :product_id,
+        Arel.sql("json_agg(json_build_array(key, value)) as properties")
+      )
+
+    Listing
+      .with(grouped_properties:)
+      .joins(:products)
+      .joins(<<~SQL)
+          left outer join grouped_properties 
+          on grouped_properties.product_id = products.id
+      SQL
+      .pluck(
+        "listings.id",
+        "listings.name",
+        "products.id",
+        "products.inventory",
+        "products.price_cents_usd",
+        "grouped_properties.properties",
+      )
+  end
 end
